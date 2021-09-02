f0fc424df8a9ae851741b01d5ba8499f2ec337e0
Olga Malkovich <olga.malkovich@gmail.com>
2021-09-02 21:10:30 +0300

    fix: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor(min, max, result) {
+        this.min = min;
+        this.max = max;
+        this.result = result;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.result = ((this.min + this.min) / 2);
+        return this.result;
     }
 
     lower() {
-
+        this.max = this.result;
     }
 
     greater() {
-
+        this.min = this.result;
     }
 }
 
